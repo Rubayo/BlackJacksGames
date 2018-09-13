2186609bbd73dc5423a79782b7621363690719d4
Rubayo <prubayo12@gmail.com>
2018-09-13 16:21:47 -0400

    Start Game
    Variables and to set the game and the push console.log

---
+++ asset/js/blackjack.js
@@ -0,0 +1,15 @@
+/*
+	BlackJack Game
+	by PR
+*/
+
+let card1 = 'Aces of Spades',
+	card2 = 'Ten of Hearts',
+ 	yourBets = 50.00;	
+
+
+console.log("Welcome to BlackJack!");
+console.log("You are deal:");
+console.log("  " + card1);
+console.log("  " + card2);
+console.log(yourBets + " " + "Chips");
