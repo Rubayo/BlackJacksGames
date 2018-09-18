7ba90cb27ed91658e39a7c0615e7d20bc4deae7f
Rubayo <prubayo12@gmail.com>
2018-09-18 17:48:27 -0400

    new code to the JS add Arrays

---
+++ blackjack.js
@@ -0,0 +1,24 @@
+/*
+	BlackJack Game
+	by PR
+*/
+
+/*
+let card1 = 'Aces of Spades',
+	card2 = 'Ten of Hearts',
+ 	yourBets = 50.00;	
+*/
+
+let deck = [
+	'Ace of Spades',
+	'Two of Spades',
+	'Tree of Spades'
+];
+
+let playerCards =[ deck[0], deck[2]];
+
+console.log("Welcome to BlackJack!");
+console.log("You are deal:");
+console.log("  " + playerCards[0]);
+console.log("  " + playerCards[1]);
+// console.log(yourBets + " " + "Chips");
