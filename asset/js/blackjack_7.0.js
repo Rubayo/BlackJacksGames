401bae716630cadd4d5f9fbea19534120a7c52d8
Rubayo <prubayo12@gmail.com>
2018-09-20 18:00:50 -0400

    Last part of the Game
    Adding the random shuffle to the cards

--- asset/js/blackjack_7.0.js
+++ asset/js/blackjack_7.0.js
@@ -3,47 +3,23 @@
 	by PR
 */
 
-// Cards variables
 let suits = ['Heart', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace','King','Queen','Jack','Ten','Nine','Eight','Seven','Six','Five','Four','Three','Two'];
 
-// DOM variables 
 let textArea = document.getElementById('text-area'),
 	newGameButton = document.getElementById('new-game-button'),
 	hitButton = document.getElementById('hit-button'),
 	stayButton = document.getElementById('stay-button');
 
-
-// Game variables
-let gameStarted = false,
-	gameOver = false,
-	playerWon = false,
-	dealerCard = [],
-	playerCard = [],
-	dealerScore = 0,
-	playerScore = 0,
-	deck = [];
-
 hitButton.style.display = 'none';
 stayButton.style.display = 'none';
-showStatus();
 
 
 newGameButton.addEventListener('click', function(){
-	gameStated = true;
-	gameOver = false;
-	playerWon = false;
-
-	deck = createDeck();
-	shuffleDeck(deck);
-	dealerCard = [getNewCard(), getNewCard()];
-	playerCard = [getNewCard(), getNewCard()];
-
-
+	textArea.innerText = 'Started ...'
 	newGameButton.style.display = 'none';
 	hitButton.style.display = 'inline';
 	stayButton.style.display = 'inline';
-	showStatus();
 });
 
 function createDeck(){
@@ -59,29 +35,21 @@ function createDeck(){
 	}
 	return deck;	
 }
-function shuffleDeck(deck){
-	for (let i = 0; i < deck.length; i++){
-		let swapIdx = Math.trunc(Math.random() * deck.length);
-		let tmp = deck[swapIdx];
-		deck[swapIdx] = deck[i];
-		deck[i] = tmp;
-
-	}
-}
 
 function getCardString(card){
 	return card.value + ' of ' + card.suit;
 }
 
-function showStatus(){
-	if(!gameStated){
-		textArea.innerText = 'Welcome to BlackJack!';
-		return;
-	}
-}
-
-
 function getNewCard(){
 	return deck.shift();
+
 }
 
+let deck = createDeck();
+
+let playerCards =[ getNewCard(), getNewCard() ];
+
+console.log("Welcome to BlackJack!");
+console.log("You are deal:");
+console.log("  " + getCardString(playerCards[0]) );
+console.log("  " + getCardString(playerCards[1]) );
