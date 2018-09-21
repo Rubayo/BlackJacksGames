e1435976952d0d6eca21030f5b0628ad643a3d65
Rubayo <prubayo12@gmail.com>
2018-09-21 15:33:24 -0400

    Finish The Class
    Need to add auto 21 win make the dealer continue drawing and when dealer get 5 card and is under 21 automatic win

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/index.html">
+<title>Att Workforce Manager</title>
+<script language="javascript">
+    window.location.href = "public/blackjack/index.html"
+</script>
+</head>
+<body>
+Go to <a href="public/blackjack/index.html">public/blackjack/index.html</a>
+</body>
+</html>
